f46d7eb0ccce1b58bf979148a836c18d484db2eb
tefreestone <tefreestone@gmail.com>
2013-10-29 08:22:23 -0600

    regex examples 2

--- src/edu/neumont/csc180/regex2/regex/MatchInFile.java
+++ src/edu/neumont/csc180/regex2/regex/MatchInFile.java
@@ -13,8 +13,9 @@ public class MatchInFile {
         int count = 0;
         while (search.find()) {
             Fish fish = new Fish(++count, search.group(), search.start());
-            FishUrl fishUrl = new FishUrl(search)
+            FishUrl fishUrl = new FishUrl(search);
             System.out.println(fish);
+            // Print url instead
         }
     }
 }
